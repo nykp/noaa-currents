2d63c17c3469cebc538dafccdccd3d98f3ad6772
Erik Schomburg <eschomburg@gmail.com>
2022-06-06 22:49:08 -0400

    change secrets dir location, and secrets path bug

--- src/nykp_currents/utils/secrets.py
+++ src/nykp_currents/utils/secrets.py
@@ -1,6 +1,9 @@
-SLACK_TOKEN_FILE_PATH = 'secrets/slack_app_token'
+import os
+
+SECRETS_DIR = os.path.abspath(os.path.join(__file__, '../../../../.secrets'))
+SLACK_TOKEN_FILE = os.path.join(SECRETS_DIR, 'slack_app_token')
 
 
 def get_slack_token():
-    with open(SLACK_TOKEN_FILE_PATH) as f:
+    with open(SLACK_TOKEN_FILE) as f:
         return f.readlines()[0]
