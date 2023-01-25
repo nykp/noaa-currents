5089317aee211f265b7552fc9b7a97cb2c64a014
Erik Schomburg <eschomburg@gmail.com>
2023-01-25 00:24:35 -0500

    make secrets utils more robust

--- src/nykp_currents/utils/secrets.py
+++ src/nykp_currents/utils/secrets.py
@@ -7,9 +7,9 @@ SLACK_USER_TOKEN_FILE = os.path.join(SECRETS_DIR, 'slack_app_user_token')
 
 def get_slack_bot_token():
     with open(SLACK_BOT_TOKEN_FILE) as f:
-        return f.readlines()[0]
+        return f.readlines()[0].strip()
 
 
 def get_slack_user_token():
     with open(SLACK_USER_TOKEN_FILE) as f:
-        return f.readlines()[0]
+        return f.readlines()[0].strip()
